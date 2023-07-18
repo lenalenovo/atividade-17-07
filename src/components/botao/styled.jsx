d775ca0338a4c610e3913c5c2499be3c0235e48d
Helena Vasconcelos <84184845+lenalenovo@users.noreply.github.com>
2023-07-18 10:57:42 -0300

    Delete src directory

--- src/components/botao/styled.jsx
+++
@@ -1,9 +0,0 @@
-
-import { styled } from "styled-components"
-export const BotaoBisneto = styled.a`
-     display: flex;
-    align-items: center;
-    justify-content: center;
-    color: black;
-    padding: 20px;
-`
